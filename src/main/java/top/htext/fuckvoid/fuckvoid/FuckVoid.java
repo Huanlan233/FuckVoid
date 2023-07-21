f8f099f93873fd6e2110e42a1aabf6f0196b6bd8
Huanlan233 <huanlan233@outlook.com>
2023-07-21 21:28:37 +0800

    Init

---
+++ src/main/java/top/htext/fuckvoid/fuckvoid/FuckVoid.java
@@ -0,0 +1,10 @@
+package top.htext.fuckvoid.fuckvoid;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FuckVoid implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/top/htext/fuckvoid/fuckvoid/client/FuckVoidClient.java
@@ -0,0 +1,32 @@
+package top.htext.fuckvoid.fuckvoid.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FuckVoidClient implements ClientModInitializer {
+    public static final Logger logger = LogManager.getLogger("FuckVoid");
+
+    @Override
+    public void onInitializeClient() {
+        logger.info("Successfully loaded. And fuck you, the void!");
+
+        // Reminded player.
+        ClientTickEvents.END_CLIENT_TICK.register((MinecraftClient client) -> {
+            var player = client.player;
+
+            if (player != null && !player.isCreative() && !player.isSpectator() && player.getY() < 0) {
+                client.inGameHud.setTitleTicks(0, 1, 0);
+                client.inGameHud.setTitle(new TranslatableText("title.fuckvoid.warning").formatted(Formatting.RED));
+                player.playSound(new SoundEvent(new Identifier(Identifier.DEFAULT_NAMESPACE, "entity.arrow.hit_player")), SoundCategory.AMBIENT, 1.0f, 1.0f);
+            }
+        });
+    }
+}
