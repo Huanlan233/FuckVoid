3937e6b0161fbd0b4ca7f0f5d79f2189f18c4052
Huanlan233 <huanlan233@outlook.com>
2023-08-02 16:26:31 +0800

    fix: Refactor the code

--- src/main/java/top/htext/fuckvoid/fuckvoid/client/FuckVoidClient.java
+++ src/main/java/top/htext/fuckvoid/fuckvoid/client/FuckVoidClient.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
@@ -23,9 +24,8 @@ public class FuckVoidClient implements ClientModInitializer {
             var player = client.player;
 
             if (player != null && !player.isCreative() && !player.isSpectator() && player.getY() < 0) {
-                client.inGameHud.setTitleTicks(0, 1, 0);
-                client.inGameHud.setTitle(new TranslatableText("title.fuckvoid.warning").formatted(Formatting.RED));
-                player.playSound(new SoundEvent(new Identifier(Identifier.DEFAULT_NAMESPACE, "entity.arrow.hit_player")), SoundCategory.AMBIENT, 1.0f, 1.0f);
+                client.inGameHud.setTitles(new TranslatableText("title.fuckvoid.warning").formatted(Formatting.RED),null,0,1,0);
+                player.playSound(new SoundEvent(new Identifier("minecraft", "entity.arrow.hit_player")), SoundCategory.AMBIENT, 1.0f, 1.0f);
             }
         });
     }
